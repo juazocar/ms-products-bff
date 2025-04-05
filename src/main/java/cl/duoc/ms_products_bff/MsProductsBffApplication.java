096a60c6452e31797d0449352e6b8d529e91d0b4
juazocar <juazocar@profesor.duoc.cl>
2025-04-05 00:42:15 -0300

    mensaje commit

---
+++ src/main/java/cl/duoc/ms_products_bff/MsProductsBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_products_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsBffApplication.class, args);
+	}
+
+}
